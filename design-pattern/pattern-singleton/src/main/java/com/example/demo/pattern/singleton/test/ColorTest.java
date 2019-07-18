674c4b6e9354aba5c4b1023cc937696468195976
peter <610002997@qq.com>
2019-07-19 00:47:34 +0800

    设计模式-单例模式

---
+++ design-pattern/pattern-singleton/src/main/java/com/example/demo/pattern/singleton/test/ColorTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.pattern.singleton.test;
+
+
+import com.example.demo.pattern.singleton.register.Color;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by Tom on 2018/3/7.
+ */
+public class ColorTest {
+    public static void main(String[] args) {
+        System.out.println(Color.RED);
+    }
+}
