84b0fac725c1393cc4fe9146692dbfb9c19570c8
peter <610002997@qq.com>
2019-07-25 00:12:26 +0800

    设计模式-模板方法模式

---
+++ design-pattern/pattern-template/src/main/java/com/example/demo/pattern/template/MemberDaoTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.pattern.template;
+
+import com.example.demo.pattern.template.dao.MemberDao;
+
+/**
+ * Created by Tom on 2018/3/11.
+ */
+public class MemberDaoTest {
+
+    public static void main(String[] args) {
+
+        MemberDao memberDao = new MemberDao();
+        memberDao.query();
+
+    }
+}
