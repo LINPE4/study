22fe88a4451e489542a78ffb27a6cbc65d6f0ed3
peter <610002997@qq.com>
2019-08-17 19:27:09 +0800

    dubbo 第一个简单demo

---
+++ dubbo_study/dubbo_demo/dubbo-client/src/main/java/com/gupaoedu/dubbo/App.java
@@ -0,0 +1,26 @@
+package com.gupaoedu.dubbo;
+
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+import com.alibaba.dubbo.rpc.Protocol;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("dubbo-client.xml");
+
+       //得到IGpHello的远程代理对象
+        IGpHello iGpHello = (IGpHello) context.getBean("gpHelloService");
+
+        System.out.println(iGpHello.sayHello("Mic"));
+        Thread.sleep(4000);
+
+        System.in.read();
+    }
+}
