f4d3745000f6b1a43fa4e061f282ef0d98e33f60
peter <610002997@qq.com>
2019-08-30 23:26:34 +0800

    rabbitmq    消费端的限流策略

---
+++ rabbitmq_study/rabbitmq-api/src/main/java/com/peter/rabbitmq/api/limit/MyConsumer.java
@@ -0,0 +1,35 @@
+package com.peter.rabbitmq.api.limit;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+public class MyConsumer extends DefaultConsumer {
+
+
+	private Channel channel ;
+	
+	public MyConsumer(Channel channel) {
+		super(channel);
+		this.channel = channel;
+	}
+
+	@Override
+	public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+		System.err.println("-----------consume message----------");
+		System.err.println("consumerTag: " + consumerTag);
+		System.err.println("envelope: " + envelope);
+		System.err.println("properties: " + properties);
+		System.err.println("body: " + new String(body));
+
+		//long deliveryTag,
+		// boolean multiple：true/批量签收, false/一条签收
+		channel.basicAck(envelope.getDeliveryTag(), false);
+		
+	}
+
+
+}
