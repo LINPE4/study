5a664cf319fda9b9f6a47ef1b6e2531ed8e8851e
peter <610002997@qq.com>
2019-09-01 12:12:27 +0800

    rabbitmq    RabbitMQ与SpringBoot2.0整合

---
+++ rabbitmq_study/rabbitmq-springboot-producer/src/main/java/com/peter/rabbitmq/config/MainConfig.java
@@ -0,0 +1,10 @@
+package com.peter.rabbitmq.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.peter.rabbitmq.*"})
+public class MainConfig {
+
+}
