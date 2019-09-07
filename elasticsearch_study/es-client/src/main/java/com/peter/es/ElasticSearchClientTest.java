b48ed1a3b484de206d0d1f32ae49c1518cd982fc
peter <610002997@qq.com>
2019-09-07 15:33:16 +0800

    elasticsearch  向索引库中添加文档

--- elasticsearch_study/es-client/src/main/java/com/peter/es/ElasticSearchClientTest.java
+++ elasticsearch_study/es-client/src/main/java/com/peter/es/ElasticSearchClientTest.java
@@ -101,5 +101,29 @@ public class ElasticSearchClientTest {
         client.close();
     }
 
-
+    @Test
+    public void testAddDocument() throws Exception {
+        //创建一个client对象
+        //创建一个文档对象
+        XContentBuilder builder = XContentFactory.jsonBuilder()
+                .startObject()
+                .field("id",2l)
+                .field("title","北方入秋速度明显加快 多地降温幅度最多可达10度22222")
+                .field("content", "阿联酋一架客机在纽约机场被隔离 10名乘客病倒")
+                .endObject();
+        //把文档对象添加到索引库
+        client.prepareIndex()
+                //设置索引名称
+                .setIndex("index_hello")
+                //设置type
+                .setType("article")
+                //设置文档的id，如果不设置的话自动的生成一个id
+                .setId("2")
+                //设置文档信息
+                .setSource(builder)
+                //执行操作
+                .get();
+        //关闭客户端
+        client.close();
+    }
 }
