b2edf996b8fa5348728ce88d9a41df6c1e23ba03
peter <610002997@qq.com>
2019-09-07 15:31:38 +0800

    elasticsearch  使用Java客户端设置mapping步骤

--- elasticsearch_study/es-client/src/main/java/com/peter/es/ElasticSearchClientClusterTest.java
+++ elasticsearch_study/es-client/src/main/java/com/peter/es/ElasticSearchClientClusterTest.java
@@ -50,5 +50,74 @@ public class ElasticSearchClientClusterTest {
         client.close();
     }
 
+    @Test
+    public void setMappings() throws Exception {
+        //创建一个Settings对象
+        Settings settings = Settings.builder()
+                .put("cluster.name", "my-elasticsearch")
+                .build();
+        //创建一个TransPortClient对象
+        TransportClient client = new PreBuiltTransportClient(settings)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9301))
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9302))
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9303));
+        //创建一个Mappings信息
+        /*{
+            "article":{
+            "properties":{
+                "id":{
+                    "type":"long",
+                            "store":true
+                },
+                "title":{
+                    "type":"text",
+                            "store":true,
+                            "index":true,
+                            "analyzer":"ik_smart"
+                },
+                "content":{
+                    "type":"text",
+                            "store":true,
+                            "index":true,
+                            "analyzer":"ik_smart"
+                }
+            }
+        }
+        }*/
+        XContentBuilder builder = XContentFactory.jsonBuilder()
+                .startObject()
+                    .startObject("article")
+                        .startObject("properties")
+                            .startObject("id")
+                                .field("type", "long")
+                                .field("store", true)
+                            .endObject()
+                            .startObject("title")
+                                .field("type", "text")
+                                .field("store", true)
+                                .field("analyzer", "ik_smart")
+                            .endObject()
+                            .startObject("content")
+                                .field("type", "text")
+                                .field("store", true)
+                                .field("analyzer", "ik_smart")
+                            .endObject()
+                        .endObject()
+                    .endObject()
+                .endObject();
+        //使用client把mapping信息设置到索引库中
+        client.admin().indices()
+                //设置要做映射的索引
+                .preparePutMapping("index_hello")
+                //设置要做映射的type
+                .setType("article")
+                //mapping信息，可以是XContentBuilder对象可以是json格式的字符串
+                .setSource(builder)
+                //执行操作
+                .get();
+        //关闭链接
+        client.close();
+    }
+
 
 }
--- elasticsearch_study/es-client/src/main/java/com/peter/es/ElasticSearchClientTest.java
+++ elasticsearch_study/es-client/src/main/java/com/peter/es/ElasticSearchClientTest.java
@@ -3,6 +3,8 @@ package com.peter.es;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +14,15 @@ import java.net.InetAddress;
 public class ElasticSearchClientTest {
 
     private TransportClient client;
+
+    @Before
+    public void init() throws Exception {
+        //创建一个Settings对象
+        //创建一个TransPortClient对象
+        client = new PreBuiltTransportClient(Settings.EMPTY)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("192.168.140.128"), 9300));
+
+    }
 
     @Test
     public void createIndex() throws Exception {
@@ -24,6 +35,69 @@ public class ElasticSearchClientTest {
                 //执行操作
                 .get();
         //4、关闭client对象
+        client.close();
+    }
+
+    @Test
+    public void setMappings() throws Exception {
+        //创建一个TransPortClient对象
+        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("192.168.140.128"), 9300));
+        //创建一个Mappings信息
+        /*{
+            "article":{
+            "properties":{
+                "id":{
+                    "type":"long",
+                            "store":true
+                },
+                "title":{
+                    "type":"text",
+                            "store":true,
+                            "index":true,
+                            "analyzer":"ik_smart"
+                },
+                "content":{
+                    "type":"text",
+                            "store":true,
+                            "index":true,
+                            "analyzer":"ik_smart"
+                }
+            }
+        }
+        }*/
+        XContentBuilder builder = XContentFactory.jsonBuilder()
+                .startObject()
+                    .startObject("article")
+                        .startObject("properties")
+                            .startObject("id")
+                                .field("type", "long")
+                                .field("store", true)
+                            .endObject()
+                            .startObject("title")
+                                .field("type", "text")
+                                .field("store", true)
+                                .field("analyzer", "ik_smart")
+                            .endObject()
+                            .startObject("content")
+                                .field("type", "text")
+                                .field("store", true)
+                                .field("analyzer", "ik_smart")
+                            .endObject()
+                        .endObject()
+                    .endObject()
+                .endObject();
+        //使用client把mapping信息设置到索引库中
+        client.admin().indices()
+                //设置要做映射的索引
+                .preparePutMapping("index_hello")
+                //设置要做映射的type
+                .setType("article")
+                //mapping信息，可以是XContentBuilder对象可以是json格式的字符串
+                .setSource(builder)
+                //执行操作
+                .get();
+        //关闭链接
         client.close();
     }
 
