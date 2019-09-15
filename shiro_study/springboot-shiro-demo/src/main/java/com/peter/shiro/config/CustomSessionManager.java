00fc718eb7781c5cd091479906eb3b4d91ba4b64
peter <610002997@qq.com>
2019-09-15 22:45:36 +0800

    shiro  ShiroFilterFactoryBean配置实战

---
+++ shiro_study/springboot-shiro-demo/src/main/java/com/peter/shiro/config/CustomSessionManager.java
@@ -0,0 +1,8 @@
+package com.peter.shiro.config;
+
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+
+public class CustomSessionManager extends DefaultWebSessionManager {
+
+    
+}
