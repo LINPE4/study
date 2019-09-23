b57388899d2cfdbc32a49111e3183791ccc8bb07
peter <610002997@qq.com>
2019-09-23 23:54:29 +0800

    springboot  配置-@ConfigurationProperties与@Value区别

--- springboot_study/core-demo/spring-boot-02-config/src/main/java/com/peter/springboot/bean/Person.java
+++ springboot_study/core-demo/spring-boot-02-config/src/main/java/com/peter/springboot/bean/Person.java
@@ -1,9 +1,14 @@
 package com.peter.springboot.bean;
 
 
+import org.hibernate.validator.constraints.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Null;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -18,15 +23,29 @@ import java.util.Map;
  *  @ConfigurationProperties(prefix = "person")默认从全局配置文件中获取值；
  *
  */
+//@PropertySource(value = {"classpath:person.properties"})
 @Component
 @ConfigurationProperties(prefix = "person")
+//@Validated
 public class Person {
 
+    /**
+     * <bean class="Person">
+     *      <property name="lastName" value="字面量/${key}从环境变量、配置文件中获取值/#{SpEL}"></property>
+     * <bean/>
+     */
+
+   //lastName必须是邮箱格式
+   // @Email
+    //@Value("${person.last-name}")
     private String lastName;
+    //@Value("#{11*2}")
     private Integer age;
+    //@Value("true")
     private Boolean boss;
 
     private Date birth;
+    //@Value("${person.maps}")
     private Map<String,Object> maps;
     private List<Object> lists;
     private Dog dog;
