6e693359f92da303e30b817bbf3823e4f11e4c64
peter <610002997@qq.com>
2019-09-25 23:17:31 +0800

    springboot  web开发-扩展与全面接管SpringMVC

---
+++ springboot_study/core-demo/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -0,0 +1,37 @@
+package com.atguigu.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Locale;
+
+//使用WebMvcConfigurerAdapter可以来扩展SpringMVC的功能
+//@EnableWebMvc   不要接管SpringMVC
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver myViewReolver(){
+        return new MyViewResolver();
+    }
+
+    public static class MyViewResolver implements ViewResolver{
+
+        @Override
+        public View resolveViewName(String viewName, Locale locale) throws Exception {
+            return null;
+        }
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+       // super.addViewControllers(registry);
+        //浏览器发送 /atguigu 请求来到 success
+        registry.addViewController("/atguigu").setViewName("success");
+    }
+
+}
