5f45929b678bd208dd12a7f95970725b526b94f7
peter <610002997@qq.com>
2019-10-09 21:51:58 +0800

    springboot  web开发-注册servlet三大组件

--- springboot_study/core-demo/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyServerConfig.java
+++ springboot_study/core-demo/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyServerConfig.java
@@ -1,9 +1,17 @@
 package com.atguigu.springboot.config;
 
+import com.atguigu.springboot.filter.MyFilter;
+import com.atguigu.springboot.listener.MyListener;
+import com.atguigu.springboot.servlet.MyServlet;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class MyServerConfig {
@@ -19,6 +27,28 @@ public class MyServerConfig {
                 container.setPort(8083);
             }
         };
+    }
+
+    //注册三大组件
+    @Bean
+    public ServletRegistrationBean myServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(),"/myServlet");
+        registrationBean.setLoadOnStartup(1);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean myFilter(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new MyFilter());
+        registrationBean.setUrlPatterns(Arrays.asList("/hello","/myServlet"));
+        return registrationBean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean myListener(){
+        ServletListenerRegistrationBean<MyListener> registrationBean = new ServletListenerRegistrationBean<>(new MyListener());
+        return registrationBean;
     }
 
 }
