c5106eeedea3a96465202bd6e15aa67e5c2ad9d3
peter <610002997@qq.com>
2019-10-09 22:49:16 +0800

    springboot  web开发-使用外部Servlet容器&JSP支持

---
+++ springboot_study/core-demo/spring-boot-04-web-jsp2/src/main/java/com/peter/springboot/SpringBoot04WebJsp2Application.java
@@ -0,0 +1,16 @@
+package com.peter.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  不要直接运行， 需要执行 spring-boot:run
+ */
+@SpringBootApplication
+public class SpringBoot04WebJsp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot04WebJsp2Application.class, args);
+	}
+
+}
