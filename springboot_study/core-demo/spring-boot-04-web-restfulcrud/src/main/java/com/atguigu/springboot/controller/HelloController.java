555f49649111a146501b3e8b140a627c1ba787dd
peter <610002997@qq.com>
2019-10-09 21:40:06 +0800

    springboot  web开发-【实验】-定制错误数据

--- springboot_study/core-demo/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ springboot_study/core-demo/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.atguigu.springboot.controller;
 
 
+import com.atguigu.springboot.exception.UserNotExistException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +15,13 @@ public class HelloController {
 //        return "index";
 //    }
 
-
+    // http://localhost:8081/crud/hello?user=aaa
+    @ResponseBody
+    @RequestMapping("/hello")
+    public  String hello(@RequestParam("user") String user){
+        if(user.equals("aaa")){
+            throw new UserNotExistException();
+        }
+        return "Hello World";
+    }
 }
