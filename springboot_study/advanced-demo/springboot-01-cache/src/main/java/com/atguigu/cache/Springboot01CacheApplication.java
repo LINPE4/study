19b1ff62d6ff78ba850b5b918cb7b3767bee94e7
peter <610002997@qq.com>
2019-10-12 00:09:37 +0800

    springboot  缓存-基本环境搭建

---
+++ springboot_study/advanced-demo/springboot-01-cache/src/main/java/com/atguigu/cache/Springboot01CacheApplication.java
@@ -0,0 +1,24 @@
+package com.atguigu.cache;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * 一、搭建基本环境
+ * 1、导入数据库文件 创建出department和employee表
+ * 2、创建javaBean封装数据
+ * 3、整合MyBatis操作数据库
+ * 		1.配置数据源信息
+ * 		2.使用注解版的MyBatis；
+ * 			1）、@MapperScan指定需要扫描的mapper接口所在的包
+ */
+@MapperScan("com.atguigu.cache.mapper")
+@SpringBootApplication
+public class Springboot01CacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot01CacheApplication.class, args);
+	}
+}
---
+++ springboot_study/advanced-demo/springboot-01-cache/src/main/java/com/atguigu/cache/service/EmployeeService.java
@@ -0,0 +1,39 @@
+package com.atguigu.cache.service;
+
+import com.atguigu.cache.bean.Employee;
+import com.atguigu.cache.mapper.EmployeeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    public Employee getEmp(Integer id){
+        System.out.println("查询"+id+"号员工");
+        Employee emp = employeeMapper.getEmpById(id);
+        return emp;
+    }
+
+    public Employee updateEmp(Employee employee){
+        System.out.println("updateEmp:"+employee);
+        employeeMapper.updateEmp(employee);
+        return employee;
+    }
+
+    public void deleteEmp(Integer id){
+        System.out.println("deleteEmp:"+id);
+        //employeeMapper.deleteEmpById(id);
+        int i = 10/0;
+    }
+
+    public Employee getEmpByLastName(String lastName){
+        return employeeMapper.getEmpByLastName(lastName);
+    }
+
+
+
+
+}
