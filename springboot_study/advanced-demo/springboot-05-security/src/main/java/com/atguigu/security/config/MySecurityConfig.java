011a082132368af5d55572cd10aa65dea122774d
peter <610002997@qq.com>
2019-10-15 23:54:55 +0800

    springboot  安全-记住我&定制登陆页

--- springboot_study/advanced-demo/springboot-05-security/src/main/java/com/atguigu/security/config/MySecurityConfig.java
+++ springboot_study/advanced-demo/springboot-05-security/src/main/java/com/atguigu/security/config/MySecurityConfig.java
@@ -18,7 +18,8 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/level3/**").hasRole("VIP3");
 
         //开启自动配置的登陆功能，效果，如果没有登陆，没有权限就会来到登陆页面
-        http.formLogin();
+        http.formLogin().usernameParameter("user").passwordParameter("pwd")
+                .loginPage("/userlogin");
         //1、/login来到登陆页
         //2、重定向到/login?error表示登陆失败
         //3、更多详细规定
@@ -30,7 +31,10 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         //1、访问 /logout 表示用户注销，清空session
         //2、注销成功会返回 /login?logout 页面；
 
-
+        //开启记住我功能
+        http.rememberMe().rememberMeParameter("remeber");
+        //登陆成功以后，将cookie发给浏览器保存，以后访问页面带上这个cookie，只要通过检查就可以免登录
+        //点击注销会删除cookie
     }
 
     //定义认证规则
