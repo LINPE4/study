08b567e6bf8a596428ca055f162cfde7184b67c3
linlipeng <610002997@qq.com>
2020-02-11 22:32:21 +0800

    rocket mq 与spring boot整合

---
+++ rocketmq-study/xx_project/rocket-with-springboot/src/main/java/cn/enjoyedu/RocketWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package cn.enjoyedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketWithSpringbootApplication {
+
+    public static void main(String[] args) {
+        //默认情况下，启动Tomcat(8080)
+        SpringApplication.run(RocketWithSpringbootApplication.class, args);
+    }
+}
