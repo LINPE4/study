609ae6d73b34f375c1223589f97a57b2bf2a3beb
linlipeng <610002997@qq.com>
2020-07-29 14:23:37 +0800

    服务续约保活配置， Eureka健康监测

--- springcloud-study/xx_project/lesson-springcloud-2/micro-web/src/main/java/com/xiangxue/jack/controller/UserController.java
+++ springcloud-study/xx_project/lesson-springcloud-2/micro-web/src/main/java/com/xiangxue/jack/controller/UserController.java
@@ -2,6 +2,7 @@ package com.xiangxue.jack.controller;
 
 import com.xiangxue.jack.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,14 @@ public class UserController {
     @RequestMapping("/queryUser")
     public String queryUser() {
         return userService.queryContents();
+    }
+
+    public static boolean canVisitDb = true;
+    /*
+     * 这个接口只为了检测db连接是否ok
+     * */
+    @RequestMapping("/db/{can}")
+    public void setDb(@PathVariable boolean can) {
+        canVisitDb = can;
     }
 }
