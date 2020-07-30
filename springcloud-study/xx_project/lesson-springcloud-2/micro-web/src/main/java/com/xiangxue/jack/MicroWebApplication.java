cd3fcea4d2ccecaf1e0a83bcd7453f7ec0192fe9
linlipeng <610002997@qq.com>
2020-07-30 11:53:02 +0800

    熔断降级组件hystrix 使用

--- springcloud-study/xx_project/lesson-springcloud-2/micro-web/src/main/java/com/xiangxue/jack/MicroWebApplication.java
+++ springcloud-study/xx_project/lesson-springcloud-2/micro-web/src/main/java/com/xiangxue/jack/MicroWebApplication.java
@@ -2,14 +2,16 @@ package com.xiangxue.jack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-@SpringBootApplication
-
+@SpringBootApplication(scanBasePackages = {"com.xiangxue.jack"})
+//注册到eureka
 @EnableEurekaClient
+//开启断路器功能
+@EnableCircuitBreaker
 public class MicroWebApplication {
 
     @Bean
