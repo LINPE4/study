909e688ea686cc20020094c18ae4da59b8c97e39
linlipeng <610002997@qq.com>
2020-10-24 15:00:11 +0800

    滑动窗口最大值

---
+++ arithmetic-study/arithmeticdemo/src/main/java/com/peter/stack/MaxSlidingWindow.java
@@ -0,0 +1,29 @@
+/**
+ * Project: arithmeticdemo
+ * File created at 2020/10/24 14:54
+ * Copyright (c) 2018 linklogis.com all rights reserved.
+ */
+package com.peter.stack;
+
+/**
+ * 功能描述
+ *    239. 滑动窗口最大值   https://leetcode-cn.com/problems/sliding-window-maximum/
+ * @author linlipeng
+ * @version 1.0
+ * @type MaxSlidingWindow
+ * @date 2020/10/24 14:54
+ */
+public class MaxSlidingWindow {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int slideLength = nums.length - k + 1;
+        int[] result = new int[slideLength];
+        for (int i = 0; i < slideLength; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            nums[i] = max;
+        }
+        return result;
+    }
+}
