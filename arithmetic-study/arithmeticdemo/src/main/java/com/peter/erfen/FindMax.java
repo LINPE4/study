5d6c59664291cd5deb388bdd54df8297aaa59341
linlipeng <610002997@qq.com>
2020-11-10 17:30:13 +0800

    寻找旋转排序数组中的最大值

--- arithmetic-study/arithmeticdemo/src/main/java/com/peter/erfen/FindMax.java
+++ arithmetic-study/arithmeticdemo/src/main/java/com/peter/erfen/FindMax.java
@@ -7,22 +7,22 @@ package com.peter.erfen;
 
 /**
  * 功能描述
- *      153. 寻找旋转排序数组中的最大值   https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+ *      153. 寻找旋转排序数组中的最小值   https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
  * @author linlipeng
  * @version 1.0
  * @type FindMin
  * @date 2020/11/10 16:07
  */
-public class FindMin {
-    public static int findMin(int[] nums) {
+public class FindMax {
+    public static int findMax(int[] nums) {
         int left = 0, right = nums.length - 1;
         while (left < right) {
             int middle = (left + right) / 2;
-            if (nums[middle] < nums[right]) {
-                // middle可能是最小值(直接在此区间查找left-middle)
+            if (nums[middle] > nums[right]) {
+                // middle可能是最大值（直接在此区间查找left-middle）
                 right = middle;
             } else {
-                // middle肯定不是最小值(换另一区间查找middle+1-right)
+                // middle肯定不是最大值（直接在另一区间查找middle+1 - right）
                 left = middle + 1;
             }
         }
@@ -34,6 +34,6 @@ public class FindMin {
                 3,4,5,1,2
 //                4,5,6,7,0,1,2
         };
-        System.out.println(findMin(num));
+        System.out.println(findMax(num));
     }
 }
