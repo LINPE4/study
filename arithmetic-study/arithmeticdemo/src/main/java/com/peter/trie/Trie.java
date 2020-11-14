2352c2fae53c0d4fde49f695e39c3bacda00c6c7
linlipeng <610002997@qq.com>
2020-11-14 14:19:52 +0800

    实现 Trie (前缀树)

---
+++ arithmetic-study/arithmeticdemo/src/main/java/com/peter/trie/Trie.java
@@ -0,0 +1,61 @@
+/**
+ * Project: arithmeticdemo
+ * File created at 2020/11/14 14:13
+ * Copyright (c) 2018 linklogis.com all rights reserved.
+ */
+package com.peter.trie;
+
+/**
+ * 功能描述
+ *    208. 实现 Trie (前缀树)  https://leetcode-cn.com/problems/implement-trie-prefix-tree/
+ * @author linlipeng
+ * @version 1.0
+ * @type Trie
+ * @date 2020/11/14 14:13
+ */
+public class Trie {
+    private boolean isWord = false;
+    private Trie[] children = new Trie[26];
+    public Trie() {
+
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        Trie root=this;
+        char[] chars = word.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (root.children[chars[i]-'a'] == null) {
+                root.children[chars[i]-'a'] = new Trie();
+            }
+            root = root.children[chars[i]-'a'];
+        }
+        root.isWord = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Trie root=this;
+        char[] chars = word.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (root.children[chars[i]-'a'] == null) {
+                return false;
+            }
+            root = root.children[chars[i]-'a'];
+        }
+        return root.isWord;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        Trie root=this;
+        char[] chars = prefix.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (root.children[chars[i]-'a'] == null) {
+                return false;
+            }
+            root = root.children[chars[i]-'a'];
+        }
+        return true;
+    }
+}
